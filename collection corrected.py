2d8332dd4086bcf2f727b5446f07d3285621c5c4
Laleh Kalantari <57807463+lkalantari@users.noreply.github.com>
2020-02-13 12:40:34 -0600

    Update collection corrected.py

--- collection corrected.py
+++ collection corrected.py
@@ -8,7 +8,17 @@
 # William Thackeray, 1863
 # Anthony Trollope, 1882
 # Gerard Manley Hopkins, 1889
-
+#
+#Your program works but is missing some functionality originally intended.
+#Try these lines and see the difference:
+#authors = {
+#    "Charles Dickens": "1870",
+#   "William Thackeray": "1863",
+#    "Anthony Trollope": "1882",
+#    "Gerard Manley Hopkins": "1889"} (notice the close bracket was missing)
+#
+#for authors, date in authors.items():
+#   print("%s" % authors, " died in ", "%s." % date)
 
 authors = '''
     Charles Dickens, 1870
